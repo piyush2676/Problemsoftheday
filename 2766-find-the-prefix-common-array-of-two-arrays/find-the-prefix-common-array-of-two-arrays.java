daf260e6e1355f16d91cdd1a2b776975a4370833
Piyush Pandey <ppandey768500@gmail.com>
2026-05-20 19:05:03 +0530

    Time: 5 ms (46.74%) | Memory: 47.8 MB (14.01%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -11,27 +11,60 @@ class Solution {
         //         if(B[k] == A[j]) count++;
         //        }
         //     }
-        //     result[i] = count;
+        //     result[i] = count;  //TC(O(N^3))
+                                   //SC(O(1))
         // }
         // return result;
-        int n = A.length;
-        int m = B.length;
-        boolean[] isPresentA = new boolean[n+1];
-        boolean[] isPresentB = new boolean[n+1];
-        int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            isPresentA[i] = false;
-            isPresentB[i] = false;
-        }
-        for(int i=0;i<n;i++){
-            isPresentA[A[i]] = true;
-            isPresentB[B[i]] = true;
-            int count = 0;
-            for(int j=0;j<=n;j++){
-                if(isPresentA[j] == true && isPresentB[j] == true) count++;
+        // int n = A.length;
+        // int m = B.length;
+        // boolean[] isPresentA = new boolean[n+1];
+        // boolean[] isPresentB = new boolean[n+1];
+        // int[] result = new int[n];
+        // for(int i=0;i<n;i++){
+        //     isPresentA[i] = false;
+        //     isPresentB[i] = false;
+        // }
+        // for(int i=0;i<n;i++){
+        //     isPresentA[A[i]] = true;
+        //     isPresentB[B[i]] = true;
+        //     int count = 0;
+        //     for(int j=1;j<=n;j++){
+        //         if(isPresentA[j] == true && isPresentB[j] == true) count++;
+        //     }
+        //     result[i] = count;
+        // }
+        // return result;  TC(O(N^2)) 
+                           //SC(O(N))
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int count = 0;
+
+        for(int i = 0; i < A.length; i++) {
+
+            if(A[i] == B[i]) {
+                mp.put(A[i],
+                mp.getOrDefault(A[i], 0) + 2);
+                count++;
             }
-            result[i] = count;
+
+            else {
+                mp.put(A[i],
+                mp.getOrDefault(A[i], 0) + 1);
+
+                mp.put(B[i],
+                mp.getOrDefault(B[i], 0) + 1);
+
+                if(mp.get(A[i]) == 2) {
+                    count++;
+                }
+                if(mp.get(B[i]) == 2) {
+                    count++;
+                }
+            }
+
+            A[i] = count;
         }
-        return result;
+
+        return A;
     }
 }
