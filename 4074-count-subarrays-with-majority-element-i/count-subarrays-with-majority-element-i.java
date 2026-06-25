6e50287503560e67c2057d91d51188259624af64
Piyush Pandey <ppandey768500@gmail.com>
2026-06-25 22:26:23 +0530

    Time: 34 ms (66.77%) | Memory: 47.2 MB (23.38%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -12,13 +12,23 @@ class Solution {
         //     }
         // }
         // return result;
+        // int n = nums.length;
+        // int result = 0;
+        // for(int i=0;i<n;i++){
+        //     int count = 0;
+        //     for(int j=i;j<n;j++){
+        //         count+=(nums[j] == target ? 1 : 0);
+        //         if(count > (j-i+1)/2) result++;
+        //     }
+        // }
+        // return result;
         int n = nums.length;
         int result = 0;
         for(int i=0;i<n;i++){
             int count = 0;
             for(int j=i;j<n;j++){
-                count+=(nums[j] == target ? 1 : 0);
-                if(count > (j-i+1)/2) result++;
+                count+=(nums[j] == target ? +1 : -1);
+                if(count > 0) result++;
             }
         }
         return result;
