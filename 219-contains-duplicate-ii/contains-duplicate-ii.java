5d844fb52bedc965de58db9b8d30e750d9996533
Piyush Pandey <ppandey768500@gmail.com>
2026-07-11 10:38:41 +0530

    Time: 22 ms (90.97%) | Memory: 91 MB (5.27%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(mp.containsKey(nums[i])){
+                if(i-mp.get(nums[i]) <= k){
+                    return true;
+                }
+            }
+            mp.put(nums[i],i);
+        }
+        return false;
+    }
+}
